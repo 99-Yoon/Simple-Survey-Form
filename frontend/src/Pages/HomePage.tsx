ed84f8c1bac488c7e7d8e87ab7fa65beb17338cd
jang dong hyeok <eric5711@naver.com>
2022-06-24 15:46:58 +0900

    0624_final

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
 
-// type HomeProps = {
+type HomeProps = {
+    title?: string;
+};
 
-// };
-
-const HomePage = () => (
+const HomePage = ({ title = "Simple Survey Form" }: HomeProps) => (
     <div>
         <div className="text-slate-300 text-center text-3xl text-black">
             가장 쉽게 설문지를 만드세요!
         </div>
-        <div className="flex container  justify-center my-6">
-            <div className="flex  justify-center text-2xl font-bold h-14 w-28 border-blue-500 rounded-lg bg-gray-300">
+        <div className="flex container justify-center my-6">
+            <div className="flex justify-center text-2xl font-bold h-14 w-28 border-blue-500 rounded-lg bg-gray-300">
                 <div className="flex">
                     <button className="text-black">
                         +
