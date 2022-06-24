4efe97fedac765925d3bdd1e7d52df3d8b548be7
jang dong hyeok <eric5711@naver.com>
2022-06-24 15:11:25 +0900

    0624JDH

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -6,9 +6,23 @@ type HomeProps = {
 };
 
 const HomePage = ({ title = "Simple Survey Form" }: HomeProps) => (
-    <div className="text-slate-300 text-3xl font-bold">
-        {title}
+    <div>
+        <div className="text-slate-300 text-center text-3xl text-black">
+            가장 쉽게 설문지를 만드세요!
+        </div>
+        <div className="flex container  justify-center my-6">
+            <div className="flex  justify-center text-2xl font-bold h-14 w-28 border-blue-500 rounded-lg bg-gray-300">
+                <div className="flex">
+                    <button className="text-black">
+                        +
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div className="text-slate-300 text-center text-xl text-black">
+            Create now
+        </div>
     </div>
-); 
- 
+);
+
 export default HomePage;
