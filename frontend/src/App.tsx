107b6098ea1088811b038fa2921f0f46d68267ee
Jiwon Yoon@na <jiwon5393@naver.com>
2022-06-27 00:55:22 +0900

    CreateForm Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import RootPage from "./Pages/RootPage";
 import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage";
 import SignUpPage from "./Pages/SignUpPage";
+import CreateSurveyFormPage from "./Pages/CreateSurveyFormPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "tailwindcss/tailwind.css";
 
@@ -14,6 +15,7 @@ export const App = () => {
           <Route index element={<HomePage/>}/>
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignUpPage />} />
+          <Route path="create" element={<CreateSurveyFormPage/>} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/Pages/CreateSurveyFormPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "tailwindcss/tailwind.css";
+
+
+const CreateSurveyFormPage = () => (
+    <div>
+        설문조사를 직접 만들 수 있는 페이지
+    </div>
+);
+
+export default CreateSurveyFormPage;
