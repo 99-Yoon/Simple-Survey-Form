6883d5f288bb9d7c4077374a84f48e6169acff3e
jang dong hyeok <eric5711@naver.com>
2022-07-01 09:34:27 +0900

    HomePage

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,21 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 type HomeProps = {
   title?: string;
 };
 
 export const Home = ({ title = "Simple Survey Form" }: HomeProps) => (
-  <div>
-    <div className="text-center text-3xl text-black my-5">
-      가장 쉽게 설문지를 만드세요!
-    </div>
-    <div className="flex container justify-center my-6">
-      <div className="flex justify-center text-2xl font-bold h-14 w-28 border-blue-500 rounded-lg bg-gray-300">
-        <div className="flex">
-          <button className="text-black">+</button>
+  
+    <div className="flex flex-col place-items-center">
+      <div className="justify-end text-center text-3xl text-black h-16 mt-12">
+        가장 쉽게 설문지를 만드세요!
+      </div>
+      <div className="flex flex-col place-items-center container">
+        <div>
+          <Link to="/create" className="flex h-14 w-28 items-center border-2 border-themeColor font-bold text-black bg-gray-200 hover:bg-themeColor rounded-lg ">
+            <div className="text-center w-28 font-bold text-black place-items-center">+</div>
+          </Link>
         </div>
+        <p className="text-center text-xl text-black">
+          Create now!
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <img src="https://3hbgf23vu0wr11wkpae5igwe-wpengine.netdna-ssl.com/wp-content/uploads/2021/04/SurveyExample_v3.jpg"
+          className="object-scale-down justify-center"
+        />
+
       </div>
     </div>
-    <div className="text-center text-xl text-black">Create now</div>
-  </div>
+  
 );
