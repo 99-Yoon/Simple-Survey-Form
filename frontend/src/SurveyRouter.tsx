5d3b9c6f132a4c2402d385e7ad65a722fe4be7a5
Daeki Yoon <dowellware@gmail.com>
2022-07-07 10:39:36 +0900

    Profile, RequireAuth 컴포넌트 라우트에 추가

--- frontend/src/SurveyRouter.tsx
+++ frontend/src/SurveyRouter.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import { Login, SignUp } from "./auth";
+import { RequireAuth } from "./auth/RequireAuth";
 import { SurveyForm } from "./commons";
 import { CreateSurveyForm } from "./CreateSurveyForm";
 import { Home } from "./home";
+import { Profile } from "./profile";
 
 export const SurveyRouter = () => {
   return (
@@ -16,6 +18,14 @@ export const SurveyRouter = () => {
           <Route path="signup" element={<SignUp />} />
           <Route path="create" element={<CreateSurveyForm />} />
           <Route path="survey" element={<SurveyForm />} />
+          <Route
+            path="profile"
+            element={
+              <RequireAuth>
+                <Profile />
+              </RequireAuth>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
