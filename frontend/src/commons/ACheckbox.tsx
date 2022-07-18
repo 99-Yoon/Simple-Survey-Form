f067e7f11f281df1aaae432aa80dbf7b1f1915d8
SeoYeon <ivyseo0312@naver.com>
2022-07-18 16:38:30 +0900

    설문조사 유형 답변 보이기

--- frontend/src/commons/ACheckbox.tsx
+++ frontend/src/commons/ACheckbox.tsx
@@ -1,48 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { CheckboxType } from "../types";
 
-export const ACheckboxForm = () => {
+type Props = {
+  element: CheckboxType;
+};
+
+export const ACheckboxForm = ({ element }: Props) => {
   return (
-    <div className="flex flex-col container w-4/5 h-auto border-2 border-themeColor items-center m-3 py-3">
-      <div className="flex flexgi-row h-16 w-full place-content-between items-center">
-        <form className="text-xl font-bold ml-6 w-1/2">checkbox</form>
-      </div>
-      <form className="border w-11/12 my-4">설문조사 설명</form>
-      <div className="flex flex-row items-center m-3">
-        <div className="mb-4 mx-3">
-          <input
-            id="default-checkbox"
-            type="checkbox"
-            className="w-5 h-5 mt-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <input className="ml-2 text-lg font-medium">First checkbox</input>
+    <div className="flex w-full gap-4 justify-around my-3">
+      {element.content.choices.map((choice) => (
+        <div>
+          <input className="mr-2" type="checkbox" />
+          <label className="text-lg">{choice.text}</label>
         </div>
-        <div className="mb-4 mx-3">
-          <input
-            id="default-checkbox"
-            type="checkbox"
-            className="w-5 h-5 mt-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label className="ml-2 text-lg font-medium">Second checkbox</label>
-        </div>
-        <div className="mb-4 mx-3">
-          <input
-            id="default-checkbox"
-            type="checkbox"
-            value=""
-            className="w-5 h-5 mt-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label className="ml-2 text-lg font-medium">Third checkbox</label>
-        </div>
-        <div className="mb-4 mx-4">
-          <input
-            id="default-checkbox"
-            type="checkbox"
-            value=""
-            className="w-5 h-5 mt-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label className="ml-2 text-lg font-medium">Fourth checkbox</label>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
--- frontend/src/commons/ADropdown.tsx
+++ frontend/src/commons/ADropdown.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { DropdownType } from "../types";
 
-export const ADropdownForm = () => {
+type Props = {
+  element: DropdownType;
+};
+
+export const ADropdownForm = ({ element }: Props) => {
   return (
-    <div className="flex flex-col container w-4/5 h-auto border-2 border-themeColor items-center m-3 py-3">
-      <div className="flex flexgi-row h-16 w-full items-center">
-        <form className="text-xl font-bold ml-6 w-1/2">dropdown</form>
-      </div>
-      <form className="border w-11/12 my-3">설문조사 설명</form>
+    <div className="flex flex-col container w-4/5 h-auto items-center m-3 py-3">
       <select className="py-2 hover:bg-themeColor bg-gray-200 rounded ">
-        <option selected>choose answer</option>
-        <option>first</option>
-        <option>second</option>
+        {element.content.choices.map((choice) => (
+          <option>{choice.text}</option>
+        ))}
       </select>
     </div>
   );
