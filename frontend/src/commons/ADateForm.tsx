c15724fefab500e701f11baaa8534ad405a53237
SeoYeon <ivyseo0312@naver.com>
2022-07-20 14:27:35 +0900

    Adataform

--- frontend/src/commons/ADateForm.tsx
+++ frontend/src/commons/ADateForm.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ADateForm = () => {
   return (
-    <div>
+    <div className="justify-start w-11/12 m-3 py-4">
       <input type="date"></input>
     </div>
   );
