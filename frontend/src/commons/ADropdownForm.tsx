c73b1e96a288d35ece277a65e30c24325c5e1a66
SeoYeon <ivyseo0312@naver.com>
2022-07-20 14:27:09 +0900

    style 변경

--- frontend/src/commons/ADropdownForm.tsx
+++ frontend/src/commons/ADropdownForm.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 export const ADropdownForm = ({ element }: Props) => {
   return (
-    <div className="flex flex-col container w-4/5 h-auto mb-3 py-3">
-      <select className="py-2 w-48 hover:bg-themeColor bg-gray-200 rounded ">
+    <div className="flex flex-col container w-11/12 h-auto mb-3 py-3">
+      <select className="py-2 w-48 hover:bg-gray-200 border border-black rounded ">
         {element.content.choices.map((choice) => (
           <option>{choice.text}</option>
         ))}
