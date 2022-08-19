e4aab5a2241946fc55174b5ae340d24701830963
Daeki Yoon <dowellware@gmail.com>
2022-08-19 10:04:17 +0900

    home 스타일 및 로직 변경

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,37 +1,20 @@
-import React, { FormEvent } from "react";
-import { useAuth } from "../auth/auth.context";
+import React from "react";
+import { Link } from "react-router-dom";
 import homeImg from "../icons/homeImg.png";
 
 export const Home = () => {
-  const { user } = useAuth();
-
-  function clickHome(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    if (!user.isLoggedIn) {
-      console.log("버튼");
-      location.href = "/login";
-    } else {
-      location.href = "/surveys/profile";
-    }
-  }
-
   return (
-    <div className="flex flex-col place-items-center">
-      <div className="justify-end text-center text-2xl md:text-3xl text-black h-16 mt-12">
+    <div className="flex flex-col items-center">
+      <div className="text-center text-2xl md:text-3xl text-black mt-12">
         가장 쉽게 설문지를 만드세요!
       </div>
-      <div className="flex flex-col place-items-center container">
-        <div className="flex h-14 w-28 items-center border-2 font-bold text-black bg-gray-200 hover:bg-themeColor rounded-lg ">
-          <button
-            type="button"
-            className="text-center text-xl h-full w-28 font-bold hover:text-white place-items-center"
-            onClick={clickHome}
-          >
-            +
-          </button>
-        </div>
-        <p className="text-center text-xl text-black my-3">Create now!</p>
-      </div>
+      <Link
+        to={"/surveys"}
+        className="flex justify-center items-center my-6 h-12 w-28 border-2 font-bold text-black hover:text-white bg-gray-200 hover:bg-themeColor text-xl rounded-lg"
+      >
+        +
+      </Link>
+      <p className="text-center text-xl text-black mb-3">Create now!</p>
       <div className="flex mt-5 md:px-48 bg-themeColor">
         <img src={homeImg} className="m-3" />
       </div>
