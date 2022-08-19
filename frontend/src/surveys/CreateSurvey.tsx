dc16c4a2f5c1ebf01420c3875d249e57d1dd8c46
Daeki Yoon <dowellware@gmail.com>
2022-08-19 10:09:54 +0900

    컨텍스트 루트로 로직 변경

--- frontend/src/surveys/CreateSurvey.tsx
+++ frontend/src/surveys/CreateSurvey.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { surveyApi } from "../apis";
+import { useSurveys } from "../layouts";
 import { ISurvey } from "../types";
 import { ModifySurvey } from "./ModifySurvey";
 
 export const CreateSurvey = () => {
+  const { error, loading, create } = useSurveys();
+
   const surveyData = {
     _id: "",
     user: "",
@@ -12,10 +15,10 @@ export const CreateSurvey = () => {
     questions: [],
   };
 
-  const create = async (surveyData: ISurvey) => {
-    const result = await surveyApi.createSurvey(surveyData);
-    return result;
-  };
+  // const create = async (surveyData: ISurvey) => {
+  //   const result = await surveyApi.createSurvey(surveyData);
+  //   return result;
+  // };
 
   return <ModifySurvey surveyData={surveyData} callApi={create} />;
 };
--- frontend/src/surveys/EditSurvey.tsx
+++ frontend/src/surveys/EditSurvey.tsx
@@ -1,18 +1,27 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Navigate, useLocation, useParams } from "react-router-dom";
 import { surveyApi } from "../apis";
 import { ISurvey } from "../types";
 import { ModifySurvey } from "./ModifySurvey";
+import { useSurvey } from "../layouts/SurveyLayout";
 
 export const EditSurvey = () => {
-  const location = useLocation();
-  const surveyState = location.state as ISurvey;
-  console.log("edit survey:", surveyState);
+  const { survey, update } = useSurvey();
+  // const [survey, setSurvey] = useState(surveyData);
 
-  const update = async (surveyData: ISurvey) => {
-    const result = await surveyApi.updateSurvey(surveyData);
-    return result;
-  };
+  console.log("survey", survey);
+  // const location = useLocation();
+  // const surveyState = location.state as ISurvey;
+  // console.log("edit survey:", surveyState);
 
-  return <ModifySurvey surveyData={surveyState} callApi={update} />;
+  // const update = async (surveyData: ISurvey) => {
+  //   const result = await surveyApi.updateSurvey(surveyData);
+  //   return result;
+  // };
+
+  if (!survey) {
+    return <Navigate to={"/surveys"} />;
+  }
+
+  return <ModifySurvey surveyData={survey} callApi={update} />;
 };
