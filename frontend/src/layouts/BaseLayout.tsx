e6f3e05232e60ef4e0ea25b273ddbc241205c10a
Daeki Yoon <dowellware@gmail.com>
2022-11-15 16:13:06 +0900

    api error 발생을 axios interceptor 이용 처리함

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAxiosInterceptor } from "../apis";
 import { AuthProvider } from "../auth";
 import { Header } from "../commons";
 
 export const BaseLayout = () => {
+  const navigate = useNavigate();
+  useAxiosInterceptor(navigate);
+
   return (
     <AuthProvider>
       <Header />
